bc6dbd4c6c045d8e79f3800f77e10e095569589e
Lepotototor <victor.flament@epita.fr>
2026-03-31 17:35:57 +0200

    feat(format_helper): add color support

--- src/misc/format_helper.cc
+++ src/misc/format_helper.cc
@@ -1,8 +1,41 @@
 #include "format_helper.hh"
 #include "error.hh"
 
+#include <unistd.h>
+
 namespace misc
 {
+  bool tty_ = false;
+  bool tty_set = false;
+
+  bool is_a_tty()
+  {
+    if (!tty_set)
+      {
+        if (isatty(STDOUT_FILENO))
+          tty_ = true;
+        tty_set = true;
+      }
+
+    return tty_;
+  }
+
+  std::string color(int r, int g, int b)
+  {
+    std::string seq = "[38;2;";
+    seq += std::to_string(r) + ";";
+    seq += std::to_string(g) + ";";
+    seq += std::to_string(b) + ";";
+    seq += "m";
+
+    return "\x1b" + seq;
+  }
+
+  std::string apply_color(const std::string& s, const std::string color)
+  {
+    return color + s + "\033[0m";
+  }
+
   std::string bold(const std::string& s) { return "\033[1m" + s + "\033[0m"; }
 
   std::string warn(bool compile_error)
@@ -10,13 +43,23 @@ namespace misc
     if (compile_error)
       Error::nb_warn_inc();
 
-    return bold("Warning: ");
+    std::string txt = "Warning: ";
+    if (is_a_tty())
+      {
+        return apply_color(bold(txt), YELLOW);
+      }
+    else
+      return txt;
   }
 
   std::string err()
   {
     Error::nb_err_inc();
 
-    return bold("Error: ");
+    std::string txt = "Error: ";
+    if (is_a_tty())
+      return apply_color(bold(txt), RED);
+    else
+      return txt;
   }
 } // namespace misc
--- src/misc/format_helper.hh
+++ src/misc/format_helper.hh
@@ -2,8 +2,13 @@
 
 #include <string>
 
+#define BLUE color(115, 113, 252)
+#define YELLOW color(247, 221, 114)
+#define RED color(239, 71, 111)
+
 namespace misc
 {
+  std::string apply_color(const std::string& color, const std::string s);
   std::string bold(const std::string& s);
 
   std::string warn(bool compile_error = true);
