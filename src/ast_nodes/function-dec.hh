1fbb30a7ab437d449073c375af8db772a28d1fa4
Lepotototor <victor.flament@epita.fr>
2026-04-02 00:21:18 +0200

    feat(ast): add FunctionDec node

---
+++ src/ast_nodes/function-dec.hh
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "declaration.hh"
+#include "exp.hh"
+#include "type-name.hh"
+
+namespace ast
+{
+
+  class FunctionDec : public Declaration
+  {
+  public:
+    FunctionDec(const misc::Location& l,
+                const std::string& name,
+                TypeName* return_type,
+                Exp* body)
+      : Declaration(l, name)
+      , return_type_(return_type)
+      , body_(body)
+    {}
+
+    ~FunctionDec() override
+    {
+      delete return_type_;
+      delete body_;
+    }
+
+    const TypeName& return_type_get() const { return *return_type_; }
+    TypeName& return_type_get() { return *return_type_; }
+
+    const Exp* body_get() const { return body_; }
+    Exp* body_get() { return body_; }
+
+  protected:
+    TypeName* return_type_;
+    Exp* body_;
+  };
+
+} // namespace ast
